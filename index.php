2fef1ad27cbbb32a04558225dcd29daec59ce630
mike <michaelwilsonsj@hotmail.com>
2021-07-06 18:02:47 -0400

    ch3 text strings

--- index.php
+++ index.php
@@ -9,11 +9,14 @@
 <body>
     <?php
     
-    $numberFive = "Mike";
-    $newVar = 5;
+    $txtS = "Cardano is 1.40 => could it trade near ETH price around 2k?";
+    $bitPrice = 1000;
     $floatingVar = 10.4;
 
-    
+    echo "Thinking of investing in Cardano. $txtS <br>";
+    echo "---------<br>" ;
+    echo "When I was in college, 2017, a classmate said, 'Bitcoin won't be trading under $bitPrice'. As of 7/6/21 it is trading at $33,740. When he said this it was sub $800."
+
      ?>
 </body>
 </html>
